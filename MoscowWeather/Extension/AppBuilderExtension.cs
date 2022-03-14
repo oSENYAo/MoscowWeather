e9110b3c74d8fd804781f76d7824337e3ee01583
Senya <kuv99@mail.ru>
2022-03-14 05:25:36 +0300

    Add notification Manager, Exception Handler

---
+++ MoscowWeather/Extension/AppBuilderExtension.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using MoscowWeather.Core.Infrastrucrure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+
+namespace MoscowWeather.Web.Extension
+{
+    public static class AppBuilderExtension
+    {
+        public static void UseNopExceptionHandler(this IApplicationBuilder application)
+        {
+            #region text
+            application.UseExceptionHandler(handler =>
+            {
+                handler.Run(async context =>
+                {
+                    //var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+                    //if (exception == null)
+                    //    return;
+                    //Message = "The process cannot access the file 'D:\\repositivs\\MoscowWeather\\MoscowWeather\\wwwroot\\Files\\log.txt' because it is being used by another process."
+                    var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                    var exception = exceptionHandlerPathFeature.Error;
+
+                    await context.Response.WriteAsJsonAsync(new { error = exception.Message });
+                    //try
+                    //{
+                    //    //get current customer
+                    //    //var userManager = EngineContext.Current.Resolve<MithraUserManager>();
+
+                    //    //log error
+                    //    //await EngineContext.Current.Resolve<Logger>().ErrorAsync(exception.Message, exception, await userManager.GetCurrentUser());
+                    //}
+                    //finally
+                    //{
+                    //    //rethrow the exception to show the error page
+                    //    //throw exception;
+                    //    ExceptionDispatchInfo.Throw(exception);
+                    //}
+                });
+            });
+            #endregion
+        }
+    }
+}
--- MoscowWeather/Startup.cs
+++ MoscowWeather/Startup.cs
@@ -1,3 +1,4 @@
+using AspNetCoreHero.ToastNotification;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Mithra.Core.Infrastructure;
 using MoscowWeather.Core.Infrastrucrure;
 using MoscowWeather.Data;
+using MoscowWeather.Web.Extension;
 using MoscowWeather.Web.Infrastructure;
 using System.Collections.Generic;
 
@@ -29,25 +31,38 @@ namespace MoscowWeather
             string connection = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<MoscowWeatherContext>(options => options.UseLazyLoadingProxies().UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMemoryCache();
-            //_engine = EngineContext.Create();
+            _engine = EngineContext.Create();
 
             _engine.RegisterDependencies(services, new List<IDependencyRegistrar>() { new DependencyRegistrar() });
+            services.AddNotyf(x =>
+            {
+                x.IsDismissable = true;
+                x.Position = NotyfPosition.TopLeft;
+                x.DurationInSeconds = 10;
+            });
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            //_engine.ConfigureRequestPipeline(app);
+            if (!env.IsDevelopment())
             {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+            //app.UseDeveloperExceptionPage();
+
+            app.UseNopExceptionHandler();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
