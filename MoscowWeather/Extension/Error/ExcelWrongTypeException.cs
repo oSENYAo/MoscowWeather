838ce77a7f59d1ae3f2129f0fc37f251492ffedf
Senya <kuv99@mail.ru>
2022-03-17 01:07:38 +0300

    Add Second variant pagination (Id). Extension exception for wrong type excel

---
+++ MoscowWeather/Extension/Error/ExcelWrongTypeException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoscowWeather.Web.Extension.Error
+{
+    public class ExcelWrongTypeException : Exception
+    {
+        public ExcelWrongTypeException()
+        {
+        }
+
+        public ExcelWrongTypeException(string message)
+            : base(message)
+        {
+        }
+
+        public ExcelWrongTypeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ MoscowWeather/Models/Pagination/CursorParam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoscowWeather.Web.Models.Pagination
+{
+    public class CursorParam
+    {
+        public int Count { get; set; } = 50;
+        public int Cursor { get; set; } = 1;
+        public bool HasPreviosly { get; set; } = false;
+        public bool HasNext { get; set; } = false;
+    }
+}
